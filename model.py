f8a235ee7c07a67b2279f5bc60e6f660d4f83b94
Saif Akhter <130275596+sakhter313@users.noreply.github.com>
2026-01-13 18:18:29 +0530

    Update model.py

--- model.py
+++ model.py
@@ -47,11 +47,11 @@ vulnerable_mode = st.sidebar.radio(
     index=1
 )
 
-use_real_llm = st.sidebar.checkbox(
+#use_real_llm = st.sidebar.checkbox(
     "Include Real LLM Output (optional)",
     value=False
 )
-
+#
 # -------------------------------------------------
 # OWASP Dataset
 # -------------------------------------------------
